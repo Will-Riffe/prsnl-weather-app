45bea4035055d57953d870d46b7255de524cb0f4
Will Riffe <w_riffe@protonmail.com>
2023-06-14 13:56:20 -0400

    ♻️ Refactored 5-day forecast code

--- js/weather.js
+++ js/weather.js
@@ -2,7 +2,7 @@ class WeatherApp {
     constructor(apiKey) {
         this.apiKey = apiKey;
         this.form = document.querySelector("form");
-        this.search = document.querySelector("#search");
+        this.searchInput = document.querySelector("#search");
         this.recentSearches = document.querySelector("aside ul");
         this.locationName = document.querySelector("#locationName");
 
@@ -10,113 +10,112 @@ class WeatherApp {
         this.weather = document.querySelector("#weather");
         this.humidity = document.querySelector("#humidity");
         this.feelsLike = document.querySelector("#feels-like");
-        this.windSpeed = document.querySelector('#wind');
+        this.windSpeed = document.querySelector("#wind");
 
         this.form.addEventListener("submit", this.handleFormSubmit.bind(this));
     }
 
-
-
-    // static func checks the entered zip using regex expression
-    // returns boolean value
     static isZipValid(zip) {
         return /^\d{5}(-\d{4})?$/.test(zip);
     }
 
-
-
-    // static func for current date and time
     static displayTime() {
         const dateTime = new Date();
         const dateTimeEl = document.getElementById("time");
-        dateTimeEl.textContent = `${dateTime}`; //updates element with id 'time'
+        if (dateTimeEl) {
+            dateTimeEl.textContent = dateTime.toString();
+        } else {
+            console.error("Element with ID 'time' not found.");
+        }
     }
-
-
 
     handleFormSubmit(event) {
         event.preventDefault();
         const search = this.searchInput.value;
-        if (isZipValid(search)) {
+        if (WeatherApp.isZipValid(search)) {
             this.getWeather(search, 'zip');
         } else {
             this.getWeather(search, 'city');
         }
-        this.add2History(search);
-        this.displayTime();
+        this.saveSearch(search);
+        WeatherApp.displayTime();
     }
 
+
+    getForecast(cityName) {
+        const forecastPath = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${this.apiKey}&units=imperial`;
+
+        fetch(forecastPath)
+            .then((res) => res.json())
+            .then((json) => this.populateFiveDayForecast(json.list))
+    }
 
     getWeather(searchTerm, searchType) {
         let currentWeatherPath, forecastPath;
         if (searchType === 'zip') {
-            currentWeatherPath = `https://api.openweathermap.org/data/2.5/weather?zip=${searchTerm}&appid=${apiKey}&units=imperial`;
-            forecastPath = `https://api.openweathermap.org/data/2.5/forecast?zip=${searchTerm}&appid=${apiKey}&units=imperial`;
+            currentWeatherPath = `https://api.openweathermap.org/data/2.5/weather?zip=${searchTerm}&appid=${this.apiKey}&units=imperial`; // Updated apiKey reference
+            forecastPath = `https://api.openweathermap.org/data/2.5/forecast?zip=${searchTerm}&appid=${this.apiKey}&units=imperial`; // Updated apiKey reference
         } else {
-            currentWeatherPath = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=${apiKey}&units=imperial`;
-            forecastPath = `https://api.openweathermap.org/data/2.5/forecast?q=${searchTerm}&appid=${apiKey}&units=imperial`;
+            currentWeatherPath = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=${this.apiKey}&units=imperial`; // Updated apiKey reference
+            forecastPath = `https://api.openweathermap.org/data/2.5/forecast?q=${searchTerm}&appid=${this.apiKey}&units=imperial`; // Updated apiKey reference
         }
 
-        fetch()
+        fetch(currentWeatherPath)
+            .then((res) => res.json())
+            .then((json) => {
+                this.updateCurrentWeatherUI(json);
+                this.getForecast(json.name);
+            })
+            .catch((err) => console.log(err.message));
     }
-};
+
+    updateCurrentWeatherUI(json) {
+        if (currentWeather) {
+            this.weather.innerHTML = json.weather[0].description;
+            this.temperature.innerHTML = `${json.main.temp} &deg;F`;
+            this.feelsLike.innerHTML = `${json.main.feels_like} &deg;F`;
+            this.humidity.innerHTML = `${json.main.humidity}%`;
+        } else {
+            console.log("No weather data available.");
+        }
+
+    }
+    
+    populateFiveDayForecast(forecastData) {
+        const forecastArticles = document.querySelectorAll(".large-2.cell.card");
+        const days = ["today", "tomorrow", "twoDays", "threeDays", "fourDays"];
+    
+        for (let i = 0; i < forecastArticles.length; i++) {
+          const forecast = forecastData[i * 8]; // Weather data for a specific day at index i * 8
+          const article = forecastArticles[i];
+          const date = new Date(forecast.dt_txt);
+          const day = days[i];
+    
+          const header = article.querySelector("header h5");
+          const tempLi = article.querySelector("ul li:nth-child(1)");
+          const windLi = article.querySelector("ul li:nth-child(2)");
+          const humidityLi = article.querySelector("ul li:nth-child(3)");
+    
+          header.textContent = day.charAt(0).toUpperCase() + day.slice(1); // Capitalize the day
+          tempLi.textContent = `Temp: ${forecast.main.temp} °F`;
+          windLi.textContent = `Wind: ${forecast.wind.speed} mph`;
+          humidityLi.textContent = `Humidity: ${forecast.main.humidity}%`;
+        }
+      }
     
 
-fetchCityData(city) {
-    return fetch("../json/cityList.json").then((response) => response.json());
-}
-
-filterCityData(data, city, state, country) {
-    return data.filter((item) => {
-        return item.city === city && (!item.state || item.state === state) && item.country === country;
-    });
-}
-
-updateWeatherData(data, cityData) {
-    const currentWeather = data.list[0];
-
-    this.locationName.textContent = cityData.name;
-    if (currentWeather) {
-        this.weather.textContent = currentWeather.weather[0].description;
-        this.temperature.textContent = currentWeather.main.temp;
-        this.feelsLike.textContent = currentWeather.main.feels_like;
-        this.humidity.textContent = currentWeather.main.humidity;
-    } else {
-        console.log("No weather data available.");
+    saveSearch(cityName) {
+        localStorage.setItem("cityName", cityName);
     }
 
-    this.populateFiveDayForecast(data.list);
-    this.saveSearch(cityData.name);
-    this.displayRecentSearches();
-}
-
-populateFiveDayForecast(fiveDayForecast) {
-    const today = new Date();
-    for (let i = 0; i < 5; i++) {
-        const date = new Date(today);
-        date.setDate(today.getDate() + i);
-        const dateString = date.toLocaleDateString("en-US", { weekday: "long", month: "short", day: "numeric" });
-        const article = document.querySelector(`#${dateString.replace(/ /g, "")}`);
-        article.querySelector("h5").textContent = dateString;
-
-        article.querySelector("li:nth-child(1)").textContent = `Temp: ${fiveDayForecast[i].main.temp} °F`;
-        article.querySelector("li:nth-child(2)").textContent = `Wind: ${fiveDayForecast[i].wind.speed} MPH`;
-        article.querySelector("li:nth-child(3)").textContent = `Humidity: ${fiveDayForecast[i].main.humidity} %`;
+    displayRecentSearches() {
+        const savedCityName = localStorage.getItem("cityName");
+        if (savedCityName) {
+            const li = document.createElement("li");
+            li.textContent = savedCityName;
+            this.recentSearches.appendChild(li);
+        }
     }
-}
-
-saveSearch(cityName) {
-    localStorage.setItem("cityName", cityName);
-}
-
-displayRecentSearches() {
-    const savedCityName = localStorage.getItem("cityName");
-    if (savedCityName) {
-        const li = document.createElement("li");
-        li.textContent = savedCityName;
-        this.recentSearches.appendChild(li);
-    }
-}
 }
 
 const apiKey = "ed5b70ee3a26a414b68275baca4b2daa";
